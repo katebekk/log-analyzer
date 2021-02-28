328547c6aceca169ea77ee5b3a6f27ea940ed97e
Kate Bekkerman <85067@mail.ru>
2021-02-28 20:03:27 +1000

    Находит временые промежутки, нет ограничения по памяти, сортировки , флагов и тестов

--- src/analyze.php
+++ src/analyze.php
@@ -17,44 +17,42 @@ $handle = fopen('php://stdin', 'r');
 //на вход функции подается часть потока, парсится и анализируется, и печатает результат выполнения
 function check_log($file, $num, $time)
 {
-    $count_true = 0;
+    $count = 0;
     $count_false = 0;
     $arr[] = array();
     $time_start = null;
     $time_end = null;
     $flag = true;//true - осчет до пика  false - после
     while (!feof($file)) {
+        $count ++;
         $buffer = fgetcsv($file, 1000, " ");
         //проверка на пустую строку
         if(empty($buffer[0])) $buffer = fgetcsv($file, 1000, " ");
 
-        if  (($buffer[6] == 500) or ($buffer[8] > $time)) {
-            $count_false ++;
-        }else if (($buffer[6] != 500) and ($buffer[8] < $time)) {
-            $count_true ++;
-        }
-        echo $buffer[8]," ",$count_false," ",$count_true,"\n";
-//        $lvl = $count_false / $count_true * 100;//вычисление процента ошибок
-//        if ($lvl >= $num) {
-//            $time_start = $buffer[3];
-//            while (!feof($file) or ($lvl >= $num) or (($buffer[6] != 500) and ($buffer[8] < $time))) {
-//                $count_true++;
-//                $buffer = fgetcsv($file, 1000, " ");
-//                $time_end = $buffer[3];
-//            }
-//            echo $time_start," ",$time_end,"\n";
-//            array_push($arr, arrray($time_start, $time_end, $lvl));
-//        }
+        if(($buffer[6] == 500) or ($buffer[8] > $time)) $count_false ++;
 
+        $lvl = $count_false / $count * 100;//вычисление процента ошибок
+        if($lvl >= $num and $count > 1) {
+            $time_start = substr($buffer[3], 12, 21);
+            $time_end = substr($buffer[3], 12, 21);
+            while (($lvl >= $num) and (($buffer[6] != 500) or ($buffer[8] < $time)) and (!feof($file)) ) {
+
+                $buffer = fgetcsv($file, 1000, " ");
+                if(empty($buffer[0])) $buffer = fgetcsv($file, 1000, " ");
+                $count ++;
+                if(($buffer[6] == 500) or ($buffer[8] > $time)) $count_false ++;
+                $time_end = substr($buffer[3], 12, 21);
+                $lvl = $count_false / $count * 100;
+            }
+            echo  $lvl," ", $time_start," ",$time_end,"\n";
+            array_push($arr, array($time_start, $time_end, $lvl));
+
+        }
     }
-/*    echo "\n";
-    echo substr($time_start, 12, 21);//подстрока время
-    echo "\n";*/
 }
 
-check_log($handle, 45, 43);
+check_log($handle, 76, 43);
 
-//100 все 23 ошибочные 100 - 73
 
 function print_answer($arr)
 {
