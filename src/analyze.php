0a5af9405b53564daa93d19d0196cc93ebb1d833
Kate Bekkerman <85067@mail.ru>
2021-03-01 18:20:51 +1000

    some changes

--- src/analyze.php
+++ src/analyze.php
@@ -1,6 +1,13 @@
 <?php
-ini_set('auto_detect_line_endings', TRUE);
+
+$shortopts .= "u:";
+$shortopts .= "t:";
+
 $handle = fopen('php://stdin', 'r');
+
+$options = getopt($shortopts);
+$num1 = $options["u"];
+$time1 = $options["t"];
 
 
 // 6 - код ошибки
@@ -15,84 +22,79 @@ function check_log($file, $num, $time)
     $time_start = null;
     $time_end = null;
 
+
     while (!feof($file)) {
         $count++;
         $buffer = fgetcsv($file, 1000, " ");
         //проверка на пустую строку
         if (empty($buffer[0])) $buffer = fgetcsv($file, 1000, " ");
 
-        if (($buffer[6] == 500) or ($buffer[8] > $time)) $count_false++;
+        if ((preg_match('/^(5(\d\d))$/',$buffer[6])) or ($buffer[8] > $time)) $count_false++;
 
         $lvl = $count_false / $count * 100;//вычисление процента ошибок
         if ($lvl >= $num and $count > 1) {
             $time_start = substr($buffer[3], 12, 21);
             $time_end = substr($buffer[3], 12, 21);
-            while (($lvl >= $num) and (($buffer[6] != 500) or ($buffer[8] < $time)) and (!feof($file))) {
-
+            while (($lvl >= $num) and (!preg_match('/^(5(\d\d))$/',$buffer[6]) or ($buffer[8] < $time)) and (!feof($file))) {
                 $buffer = fgetcsv($file, 1000, " ");
                 if (empty($buffer[0])) $buffer = fgetcsv($file, 1000, " ");
                 $count++;
-                if (($buffer[6] == 500) or ($buffer[8] > $time)) $count_false++;
+                if (preg_match('/^(5(\d\d))$/',$buffer[6]) or ($buffer[8] > $time)) $count_false++;
                 $time_end = substr($buffer[3], 12, 21);
                 $lvl = $count_false / $count * 100;
             }
-            //echo $lvl, " ", $time_start, " ", $time_end, "\n";
-            $arr2["time_start"]=$time_start;
-            $arr2["time_end"]=$time_end;
-            $arr2["lvl"]=$lvl;
-            array_push($arr, $arr2);
-
+            array_push($arr, array($time_start, $time_end, round($lvl, 1)));
 
         }
     }
-    print_answer($arr);
-    echo "\n";
     $new_arr = sort_arr($arr);
     print_answer($new_arr);
+
 }
 
-check_log($handle, 76, 43);
 
 //выводит значения двумерного массива
 function print_answer($arr)
 {
-    foreach ($arr as &$value){
-        foreach($value as &$val) echo $val," ";
+    foreach ($arr as &$value) {
+        foreach ($value as &$val) echo $val," ";
         echo "\n";
     }
 }
 
 //callback для usort
-function cmp_sort($x, $y) {
-    return strcmp($x["time_start"], $y["time_start"]);
+function cmp_sort($x, $y)
+{
+    return strcmp($x[0], $y[0]);
 }
 
 //функция сортировки
-function sort_arr($arr){
-    usort ($arr, "cmp_sort");
+function sort_arr($arr)
+{
+    usort($arr, "cmp_sort");
     return $arr;
 }
 
+//check_log($handle, $num1, $time1);
+
+$chunk_size = (1<<24);
+$position = 0;
 //while (!feof($handle)) {
 //
-//    $str = fread($handle, 60);
-//    echo $str;
+//    $position1 = ftell($handle);
+//    $chunk = fread($handle, $chunk_size );
+//
+//    $last_lf_pos = strrpos($chunk, PHP_EOL);
+//    $pos = strpos($chunk, $findme);
+//    while($pos)
+//    $pos = strpos($, $findme);
+//   //echo $position1," ",$position2," ",$last_lf_pos,"\n";
+//    $buffer = mb_substr($chunk,0,$last_lf_pos);
+//
+//    echo "hello \n",$buffer,"\n";
+//    $buffer = NULL;
+//
 //}
 
-/*check_log($handle);
 
-
-
-$count = 0;
-while(!feof($handle)) {
-    $buffer = fgetcsv($handle, 1000, " ");
-    $num = count($buffer);
-//    echo $count++, ": ", $buffer;
-    echo "<p> $num fields in line $count: <br /></p>\n";
-    $count++;
-    for ($c=0; $c < $num; $c++) {
-        echo $c;
-        echo $buffer[$c] . "<br />\n";
-    }
-}*/
 fclose($handle);
